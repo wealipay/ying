a81b004f761805684a4948cf4ce0c3741afa5b22
wealipay <1293600794@qq.com>
2023-02-28 02:44:28 +0000

    settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,36 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig,CommonServerOptions } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
+import dotenv,{DotenvParseOutput} from 'dotenv'
+import { env } from 'process'
 
-// https://vitejs.dev/config/
 export default defineConfig((mode) => {
-  const envFileName: string = ".env";
-  const curEnvFileName: string = `${envFileName}.${mode.mode}`;
-  console.log(curEnvFileName)
-  console.log(888)
+  const envFileName: string = '.env'
+  const curEnvFileName: string = `${envFileName}.${mode.mode}`
+  const envData = fs.readFileSync(curEnvFileName)
+  const envMap:DotenvParseOutput=dotenv.parse(envData)
+  let server:CommonServerOptions={}
+  if (mode.mode === 'development') {
+    server = {
+      host:envMap.VITE_HOST,
+      port:envMap.VITE_PORT,
+      proxy: {
+        [envMap.VITE_BASE_URL]: {
+          target:envMap.VITE_PROXY_DOMAIN
+        }
+      }
+    }
+    console.log(server)
+  }
+  else if (mode.mode === 'production') {
+    server = {
+      host: envMap.VITE_HOST,
+      port:envMap.VITE_PORT
+    }
+    console.log(server)
+  }
   return {
     plugins: [vue()],
-  };
-});
+    server
+  }
+})
